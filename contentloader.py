abb07b8dcfe15c04944441dae2a2ca0e04754f22
Adrian Carpenter <adriatic.c@gmail.com>
2015-11-20 03:35:01 -0800

    Content per Dave

--- contentloader.py
+++ contentloader.py
@@ -2,7 +2,9 @@
 
 import json
 import os
+import re
 import sys
+from collections import defaultdict
 
 # Auto-install and activate a virtualenv (where available)
 if sys.version_info.major >= 3 and sys.version_info.minor >= 3:
@@ -16,7 +18,8 @@ import driveclient
 from utils import log, slugify, compact
 
 
-ROOT_FOLDER = 'Beautiful Rising Toolbox Content Editing Demo'
+#ROOT_FOLDER = 'Beautiful Rising Toolbox Content Editing Demo'
+ROOT_FOLDER = 'BR CONTENT'
 SERVICE_ACCOUNT_JSON = 'google-docs-etl-bff03cc95d8e.json'
 
 
@@ -26,12 +29,23 @@ assets_dir = os.path.join(script_dir, 'assets')
 
 
 def parse_config(data):
-    #Perform any transformations necessary before serializing config data
+    '''
+    Prepare config data for serialization and storage in the db
+    '''
     c = {}
-    c['module_types'] = data.get('module_types', {})
-    c['other_types'] = data.get('other_types', {})
-    c['doc_types'] = dict(c['module_types'].items() | c['other_types'].items())
-    c['types_with_resources'] = data.get('types_with_resources', [])
+
+    # This regex is used to mark ready-to-publish documents
+    c['publish'] = data.get('publish', r'(?i)\bdone$')
+
+    # Image folder regex
+    c['images'] = data.get('images', r'(?i)^images$')
+
+    # These types should specify a folder regex, singular and plural names
+    #TODO: Catch missing type specification info
+    c['module-types'] = mtypes = data.get('module-types', [])
+    c['other-types'] = otypes = data.get('other-types', [])
+    c['all-types'] = mtypes + otypes
+
     return c
 
 
@@ -46,9 +60,10 @@ def main():
     drive = driveclient.DriveClient('google-docs-etl', 
         service_account_json_filename=SERVICE_ACCOUNT_JSON)
 
-    # Get the root directory containing all the content types
+    # Get the root directory
     root = drive.folder(ROOT_FOLDER)
     if not root:
+        # TODO: this potential point of failure should be more robust
         log("Can't find the root folder!", fatal=True)
 
     # Get the couch client and flush the old database
@@ -62,30 +77,69 @@ def main():
             config.update(parse_config(data))
             couch['config', 'main'] = config
             break
+    else:
+        log("Can't find a config file!", fatal=True)
 
-    # Work from the assets directory
-    try:
-        os.mkdir(assets_dir)
-    except FileExistsError: pass
-    os.chdir(assets_dir)
+    # This is how we match documents which are ready to publish
+    ready_to_publish = re.compile(config['publish']).search
+
+    # Keep track of all images
+    images = []
+    image_folder = None
 
     # Download content and put it in the database
-    for folder in [f for f in root.folders if compact(f.title) in config['doc_types']]:
-        images = []
-        for document in folder.documents:
-            log("Parsing content of {}...".format(document.title))
-            content = archieml.loads(document.text)
-            content.update({'title': document.title})
-            if 'image' in content:
-                images.append(content['image'])
-            type = config['doc_types'][compact(folder.title)]
-            couch[type, document.title] = content
-        for image in folder.images:
-            if image.title in images:
+    for folder in root.folders:
+
+        # Identify the image folder for later
+        if re.search(config['images'], folder.title):
+            image_folder = folder
+
+        # This checks the folder name against all the regexes for valid
+        # module types and returns the first match or None
+        doc_type = next((T for T in config['all-types'] if re.search(T['folder'], folder.title)), None)
+        if doc_type:
+
+            # No code should ever use the type_slug but content editors may
+            # choose to use "module-type: title" over "moduletype: title" so
+            # this should be supported. 
+            type = compact(doc_type['one'])
+            type_slug = slugify(doc_type['one'])
+
+            log('\nIdentified folder "{}" containing {}...'.format(folder.title, doc_type['many']))
+
+            for document in folder.documents:
+                if ready_to_publish(document.title):
+                    content = archieml.loads(document.text)
+                    content['type'] = type
+                    content['title'] = title = content.get(type_slug, content.get(type, ''))
+                    if not title:
+                        log('"{}" needs a title. Skipping!'.format(document.title))
+                        continue
+                    log("Parsed content of {}...".format(document.title))
+
+                    if 'image' in content:
+                        images.append(content['image'])
+
+                    # Store the actual data
+                    # TODO: handle duplicate couchdb ids
+                    couch[type, title] = content
+
+    # Now that all content has been processed, download the images
+    if image_folder:
+        # Work from the assets directory
+        try:
+            os.mkdir(assets_dir)
+        except FileExistsError: pass
+        os.chdir(assets_dir)
+
+        # Let's not overdownload, eh?
+        #for image in set(image_folder.images):
+
+        for image in image_folder.images:
+            if image.title in images or image.title.rsplit('.')[0] in images:
                 log("Downloading image {}...".format(image.title))
-                #XXX: Skip for now (but the following line does work)
-                #image.save_as(image.title)
-    
+                image.save_as(image.title)
+
 
 if __name__ == '__main__':
     main()
