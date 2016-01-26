6b3a0249970ef8a220cbf85b5d2977793144d9fc
Adrian Carpenter <adriatic.c@gmail.com>
2016-01-26 15:39:37 -0800

    Update API adapter

--- apiadapter.py
+++ apiadapter.py
@@ -1,45 +1,100 @@
 #!/usr/bin/env python
+"""
+Notes to self:
+* many endpoints return a subset
+* querystring as opposed to prefix?
 
-import json
+"""
+
 import sys
 
+import couchdb
+from couchdb.http import ResourceNotFound
 from flask import Flask, Response, request, jsonify
+from flask_restful import Resource, Api
+from werkzeug.exceptions import NotFound
 
-import couchclient
+from utils import *
 
 
-PREFIX = 'api'
+PREFIX = 'api/v1'
+GOOGLE_VERIFICATION = 'google5844dd3c739be066.html'
 
 
 app = Flask(__name__)
-couch = couchclient.CouchClient('toolbox')
-config = couch['config', 'main']
+api = Api(app)
+
+couch = couchdb.Server()
+db = couch['toolbox']
+
+config = db['config:api']
 
 
-query_by_type = "function(doc){if(doc.type=='%s'){emit(doc.slug,doc)}}"
+def flatten_args(args, sep=','):
+    # Flatten a werkzeug MultiDict containing duplicate, optionally comma-
+    # separated query parameters to a dictionary of lists without duplicates
+    #   e.g.:     "?name=foo&tags=a,b&tags=c,d&tags=d"
+    #   becomes:  {'name': ['foo'], 'tags': ['a', 'c', 'b', 'd']}
+    return {k: list({i for splitL in (L.split(sep) for L in args.getlist(k)) for i in splitL})
+            for k in args}
 
 
-for plural,singular in config['doc_types'].items():
-    def get_many():
-        type = config['doc_types'][request.url_rule.endpoint]
-        try: result = json.dumps([i.value for i in couch.query(query_by_type % type)])
-        except: return '', 404
-        return Response(result, 200, mimetype='application/json')
-    app.add_url_rule('/{PREFIX}/{plural}'.format(**vars()), plural, get_many)
-
-    def get_one(id):
-        type = request.url_rule.endpoint
-        try: result = couch[type, id]
-        except: return '', 404
-        return jsonify(result)
-    app.add_url_rule('/{PREFIX}/{singular}/<id>'.format(**vars()), singular, get_one)
+# In lieu of a proper couchdb design document, here's a temporary query
+#query_by_type = "function(doc){if(doc.type=='%s'){emit(doc.slug,doc)}}"
 
 
-@app.route('/{PREFIX}/config'.format(**vars()))
-def config_():
-    return jsonify(config)
+
+
+# Get content types and create endpoints
+types = {slugify(T['one']): slugify(T['many']) for T in config['types']}
+for singular_name,plural_name in types.items():
+    
+    class Many(Resource):
+        type = singular_name
+        query = "function(doc){if(doc.type=='%s'){emit(doc.slug,doc)}}" % singular_name
+        def get(self):
+            try:
+                resources = [doc['value'] for doc in db.query(self.query)]
+            except (KeyError, ResourceNotFound): raise NotFound
+            args = flatten_args(request.args)
+            filtered = []
+            if 'tags' in args:
+                for doc in resources:
+                    pass
+                    # filtering should be generic, no?
+                    # here's where we look at the taxonomies
+                    # which should be in the config.
+                    
+                    
+                #resources = [d for d in 
+                print(args['tags'])
+            
+            return resources
+    api.add_resource(Many, '/{PREFIX}/{plural_name}'.format(**vars()), endpoint=plural_name)
+
+
+    class One(Resource):
+        type = singular_name
+        def get(self, id):
+            try:
+                resource = db['{}:{}'.format(self.type, id)]
+            except ResourceNotFound: raise NotFound
+            return resource
+    api.add_resource(One, '/{PREFIX}/{singular_name}/<id>'.format(**vars()), endpoint=singular_name)
+            
+
+# Expose the config for front-ends
+class Config(Resource):
+    def get(self):
+        return config
+api.add_resource(Config, '/{PREFIX}/config'.format(**vars()))
+
+
+@app.route(GOOGLE_VERIFICATION)
+def google_verification():
+    return 'google-site-verification: {}'.format(GOOGLE_VERIFICATION)
 
 
 if __name__ == '__main__':
     app.run(port=6969, debug='debug' in sys.argv)
-    
+
