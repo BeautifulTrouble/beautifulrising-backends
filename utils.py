9da7082bb7642538598818a0863f301949a78448
Adrian Carpenter <adriatic.c@gmail.com>
2021-04-13 21:05:04 -0700

    Italiano finally broke the contentloader

--- utils.py
+++ utils.py
@@ -137,7 +137,7 @@ def parse_archieml(text):
     text = re.sub(r'^\[[a-z]\].+$', '', text, flags=re.M)
     text = re.sub(r'\[[a-z]\]', '', text)
     # Undo some of the auto-capitalization google docs inflicts
-    return {k.lower(): v for k,v in archieml.loads(text).items() if v}
+    return {k.lower(): v for k,v in archieml.loads(text).items() if v and isinstance(v, str)}
 
 
 def google_doc_id(string):
