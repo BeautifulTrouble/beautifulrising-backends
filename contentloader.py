94ade8436fe70fd7c06acdd63c0f45c6e35c45e7
Adrian Carpenter <adriatic.c@gmail.com>
2022-03-30 04:04:18 -0700

    Use ftlangdetect for detection of more languages

--- contentloader.py
+++ contentloader.py
@@ -19,7 +19,7 @@ from urllib.parse import urljoin
 import couchdb
 import driveclient
 import jinja2
-import langdetect
+import ftlangdetect
 import requests
 from fuzzywuzzy.process import extractOne
 from icu import ListFormatter, Locale
@@ -650,20 +650,13 @@ class ContentLoader(object):
         determined from a corpus of values whose keys specify no language suffix, 
         favoring more heavily those keys specified with the configuration item
         called language-detection-weighted-keys.
-
-        Language detection is difficult to perform correctly in an automatic way. 
-        Some documents are inherently multilingual and others are inherently 
-        independent of language. This method attempts to do an intelligent thing for
-        most cases, but it could be smarter. If you find yourself questioning the 
-        process herein, by all means improve it!
         '''
 
         # Get the set of possible suffixes to weed out text irrelevant for detection
-        langdetect.detector_factory.init_factory()
-        language_suffixes = {'-'+lang for lang in langdetect.detector_factory._factory.langlist}
+        language_suffixes = {f'-{lang}' for lang in self.config['language-all']}
 
         language_default = self.config['language-default']
-        weighted_keys = {k for k in self.config['language-detection-weighted-keys']}
+        weighted_keys = {*self.config['language-detection-weighted-keys']}
         omitted_keys = {'_id', '_rev', 'type', 'slug', 'timestamp', 'translations', 
                         'document_id', 'document_link', 'document_title'}
 
@@ -680,35 +673,19 @@ class ContentLoader(object):
             if 'lang' not in content:
                 text_items = {k: r_concat(v) for k,v in content.items()
                               if k[-3:] not in language_suffixes and k not in omitted_keys}
-                
-                # Create two identical language guesses (lists) as fallback for the case when detection
-                # fails on either or both. Both are low confidence guesses of the default language, with
-                # the guess being extremely low confidence when a translation-specific key is present.
-                lang, lang_weighted = [[langdetect.language.Language(language_default,
-                                        0.5 if 'default-language-content' in content else 0.7)]] * 2
 
-                # A high confidence non-weighted guess will be squashed a little, or "normalized"
-                corpus = '\n'.join(text_items.values())
-                try:
-                    lang = langdetect.detect_langs(corpus)[:1]
-                    lang[0].prob = 1 / (1 + math.exp(-lang[0].prob))
-                    # ^ Rather than using this clumsy sigmoid function to squash high values, it might
-                    #   make more sense to compare the deviation between all guesses and the top two,
-                    #   then make a decision about the weighted guess based on this difference. Multi-
-                    #   lingal docs would have low deviation between the top two guesses when compared
-                    #   with deviation overall, and a weighted guess would break the tie.
-                except langdetect.lang_detect_exception.LangDetectException: ...
+                corpus = ' '.join(text_items.values()).replace('\n', ' ')
+                corpus_weighted = ' '.join(v for k,v in text_items.items() 
+                                           if k in weighted_keys).replace('\n', ' ')
 
-                # A "weighted guess" (based on >20 chars of weighted keys) is left as-is
-                corpus_weighted = '\n'.join(v for k,v in text_items.items() if k in weighted_keys)
+                guess = ftlangdetect.detect(corpus)
+                content['lang'] = guess['lang']
+
                 if len(corpus_weighted) > 20:
-                    try: 
-                        lang_weighted = langdetect.detect_langs(corpus_weighted)
-                    except langdetect.lang_detect_exception.LangDetectException: ...
+                    guess_weighted = ftlangdetect.detect(corpus_weighted)
+                    content['lang'] = max(guess, guess_weighted, key=lambda g: g['score'])['lang']
 
-                # And the best guess wins.
-                language = max(lang + lang_weighted).lang
-                content['lang'] = language
+                log(f"""language: guessed {content['lang']} for "{content['title']}" """)
 
         return all_content
 
@@ -903,4 +880,3 @@ class ContentLoader(object):
                         log(f'convert: asset "{file.title}"')
                         for prefix,args in self.config['asset-manipulation'].items():
                             Popen(['convert', *shlex.split(args), file.title, f'{prefix}-{file.title}'])
-
